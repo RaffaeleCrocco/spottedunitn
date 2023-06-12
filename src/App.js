028ccd562fbd5d29fb1d7532953b87c6f8aa209a
Raffaele <raffaelecrocco2000@gmail.com>
2023-06-12 20:23:40 +0200

    add layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App h-screen">hello</div>;
+  return (
+    <div className="App h-screen w-screen flex flex-col md:flex-row">
+      <div className="h-1/5 md:h-screen bg-[#F3A536] w-full md:w-1/6"></div>
+      <div className="h-screen w-full md:w-5/6">hello</div>
+    </div>
+  );
 }
 
 export default App;
